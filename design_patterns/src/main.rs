65c9b8b53514df60b953d5c2f719c30574ad3f95
Józef Podlecki <jozef.witold.podlecki@gmail.com>
2025-04-25 23:39:04 +0100

    workaround attempt

--- design_patterns/src/main.rs
+++ design_patterns/src/main.rs
@@ -1,12 +1,7 @@
-use state::{Item, Resolver};
-
 mod arc_rw_lock_wrapper;
 mod state;
 mod looper;
 
 fn main() {
     let name = String::from("test");
-    let mut resolver = Resolver::default();
-
-    resolver.add(Item { name: &name });
 }
--- design_patterns/src/state.rs
+++ design_patterns/src/state.rs
@@ -5,41 +5,46 @@ pub struct Item {
     value: u64,
 }
 
+pub struct Items(HashMap<u64, Item>);
+
+impl Items {
+    pub fn get(&mut self, id: u64) -> &mut Item {
+        match self.0.entry(id) {
+            Entry::Occupied(occupied_entry) => occupied_entry.into_mut(),
+            Entry::Vacant(vacant_entry) => vacant_entry.insert(Item { name: id.to_string(), value: 0 }),
+        }
+    }
+}
+
 pub struct State {
-    items: HashMap<u64, Item>,
+    items: Items
 }
 
 impl State {
     pub fn new() -> Self {
         let mut items = HashMap::new();
         items.insert(1, Item { name: "1".to_string(), value: 1 });
+        let items = Items(items);
+
         Self { items }
     }
 
-    pub fn get_item(&mut self, id: u64) -> &mut Item {
-        match self.items.entry(id) {
-            Entry::Occupied(occupied_entry) => occupied_entry.into_mut(),
-            Entry::Vacant(vacant_entry) => vacant_entry.insert(Item { name: id.to_string(), value: 0 }),
-        }
-    }
-
-    pub fn further_modify(&mut self, item: &Item) {
+    pub fn further_modify(&mut self, value: u64) {
 
     }
 }
 
 pub fn update_state(state: &mut State) {
-    let item = state.get_item(1);
+    let items = &mut state.items;
+    let item = items.get(1);
     item.value += 10;
 
-    state.further_modify(item);
+    let value = item.value;
+    state.further_modify(value);
 }
 
 fn main() {
     let mut state = State::new();
     update_state(&mut state);
 
-    for (id, item) in &state.items {
-        println!("Item ID: {}, Name: {}, Value: {}", id, item.name, item.value);
-    }
 }
