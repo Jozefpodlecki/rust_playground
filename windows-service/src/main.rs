0768362c099f2be3abf8b3b04142e5126075205f
Józef Podlecki <jozef.witold.podlecki@gmail.com>
2025-09-13 23:53:44 +0100

    windows service, ps1 teardown code

--- windows-service/src/main.rs
+++ windows-service/src/main.rs
@@ -1,16 +1,20 @@
 #[macro_use]
 extern crate windows_service;
 
-use std::ffi::OsString;
+use std::{ffi::OsString, fs::File, io::Write, path::PathBuf};
 use windows_service::service_dispatcher;
 
-define_windows_service!(ffi_service_main, my_service_main);
+define_windows_service!(ffi_service_main, service_main);
 
-fn my_service_main(arguments: Vec<OsString>) {
-    println!("test");
+fn service_main(arguments: Vec<OsString>) {
+    let base_path = PathBuf::from(r"C:\repos\rust_playground\windows-service\target\debug");
+    let mut file = File::create(base_path.join("from-service.txt")).unwrap();
+    
+    file.write_all(b"test").unwrap();
+    file.write_all(format!("args: {:?}\n", arguments).as_bytes()).unwrap();
 }
 
 fn main() -> Result<(), windows_service::Error> {
-    service_dispatcher::start("myservice", ffi_service_main)?;
+    service_dispatcher::start("CustomService", ffi_service_main)?;
     Ok(())
 }
